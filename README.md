6cd902ce2dc1a65d8de4244b251381d69d085bf8
opesnoir <goodsurroundsme@outlook.com>
2024-04-16 10:10:41 +0200

    feat if statement

--- README.md
+++ README.md
@@ -5,5 +5,16 @@ Programma
        if else statement
        nested if
        multi way (if-else if)
-1. Logische operatoren
-2. Switch statement
+2. Logische operatoren
+3. Switch statement
+
+Code is modulair
+Een module onderdeel is een klasse
+
+Vergelijkings operatoren
+1. == betekent: is gelijk aan (waar als de items gelijk zijn)
+2. != betekent: is niet gelijk aan (waar als de items niet gelijk zijn)
+3. > betekent: is groter dan (waar als de waarde links groter is dan de waarde rechts)
+4. < betekent: is kleiner dan (waar als de linkerwaarde kleiner is dan de rechterwaarde)
+5. >= betekent: is groter dan of gelijk aan (waar als de linkerwaarde groter dan of gelijk is aan de rechterwaarde)
+6. <= betekent: is kleiner dan of gelijk aan (waar als de linkerwaarde kleiner dan of gelijk is aan de rechterwaarde)
---
+++ src/ifStatements.java
@@ -0,0 +1,36 @@
+//Functie stroomschema maken om inzichtelijk te maken wat de code moet doen
+//Boolean is een datatype dat twee waarde kent: iets is waar (true) of niet waar (false)
+
+public class ifStatements {
+    public static void main(String[] args) {
+        // je print eerst de waarde van de boolean, die is op dit punt true, gezien dat de waarde is die je hebt opgegeven
+        boolean isWaar = true;
+        System.out.println("Is het waar = " + isWaar);
+
+        // vanaf hier geef je waarden op, en zal het programma kijken of hetgeen je vraagt 'true' of 'false' is.
+        int getal1 = 17;
+        int getal2 = 20;
+
+        // isWaar ga je vullen en krijgt isWaar een waarde en je vraagt is getal1 gelijk aan of groter dan getal2
+        isWaar = (getal1 >= getal2);
+        System.out.println("Is het waar = " + isWaar);
+
+        // if statement
+        int getal = 12;
+
+        if (getal > 0){
+            System.out.println("Het opgegeven getal is positief");
+        }
+
+        // if else statement
+        // je vraagt is het opgegeven getal deelbaar is door het getal 2
+        if (getal % 2 == 0){
+            System.out.println("het getal is even");
+        } else {
+            System.out.println("het getal is oneven");
+        }
+    }
+
+
+
+}
