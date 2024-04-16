66bb9cd027408fc0ae8b480884ab06afecf21e50
opesnoir <goodsurroundsme@outlook.com>
2024-04-16 12:22:33 +0200

    switch statement met woorden

--- src/switchstatmentMetWoorden.java
+++ src/switchstatmentMetWoorden.java
@@ -1,30 +1,29 @@
 import java.util.Scanner;
 
+//switch statement met woorden en dus het gebruiken van " "
 public class switchstatmentMetWoorden {
 
     public static void main(String[] args) {
-        String mars = "mars";
-        String snicker = "snicker";
-        String milkyway = "milkeyway";
         //initieer scanner: Scanner scannerNaam = new Scanner(System.in);
         Scanner gebruikersInvoer = new Scanner(System.in);
         // de gebruiker vragen om invoer
-        System.out.println("Wil je een snicker kies 1. Wil je liever een mars kies 2. Wil je een snicker kies 3.");
+        System.out.println("Wil je een snicker, mars of milkeyway?");
 
         // maak een variabel om de waarde die de gebruiker invoert in op te bergen
         String ingevoerdeKeuze = gebruikersInvoer.nextLine(); // om een string op te halen gebruik je nextLine();
 
-        switch (ingevoerdeKeuze){
-            case mars:
+        switch (ingevoerdeKeuze) {
+            case "mars":
                 System.out.println("Je krijgt een mars.");
                 break;
-            case snicker:
+            case "snicker":
                 System.out.println("Je krijgt een snicker.");
                 break;
-            case milkyway:
+            case "milkyway":
                 System.out.println("Je krijgt een milky way.");
                 break;
             default:
-                System.out.println("Je krijgt helaas niks, omdat je niet hebt gekozen voor een geldige invoer. Kies 1, 2 of 3.");
+                System.out.println("Je krijgt helaas niks, omdat je niet hebt gekozen voor een geldige invoer. Kies mars, snicker of milkeyway.");
         }
+    }
 }
